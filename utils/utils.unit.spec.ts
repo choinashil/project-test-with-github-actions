b4faadbfe23a711293e7ff06b3a2bfabc5fbbc9d
choinashil <choinashil@gmail.com>
2022-06-27 16:48:13 +0900

    <test> 테스트 성공 케이스로 변경

--- utils/utils.unit.spec.ts
+++ utils/utils.unit.spec.ts
@@ -2,7 +2,7 @@ import { formatPrice, formatOnlyNumber } from './index';
 
 describe('formatting 유틸 함수 테스트', () => {
   it('formatPrice - 숫자 세 자리마다 콤마(,)를 붙인다', () => {
-    expect(formatPrice(3000)).toEqual('3000');
+    expect(formatPrice(3000)).toEqual('3,000');
   });
 
   it('formatOnlyNumber', () => {
