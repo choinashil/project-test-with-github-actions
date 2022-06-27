7628184b02a8cdd35cd81cc06b4688e45e7af883
choinashil <choinashil@gmail.com>
2022-06-27 16:45:46 +0900

    <test> 테스트 추가

--- utils/utils.unit.spec.ts
+++ utils/utils.unit.spec.ts
@@ -1,7 +1,11 @@
-import { formatPrice } from './index';
+import { formatPrice, formatOnlyNumber } from './index';
 
 describe('formatting 유틸 함수 테스트', () => {
   it('formatPrice - 숫자 세 자리마다 콤마(,)를 붙인다', () => {
     expect(formatPrice(3000)).toEqual('3000');
+  });
+
+  it('formatOnlyNumber', () => {
+    expect(formatOnlyNumber('asfs3242')).toEqual(3242);
   });
 });
